f73869aac4e817e88f855516ac55c0968558b8e2
Kuba W <winczukjakub@gmail.com>
2020-07-02 18:28:40 +0200

    changed lose condition

--- shm/src/game.cpp
+++ shm/src/game.cpp
@@ -42,5 +42,5 @@ bool Game::checkWinCondition() const {
 }
 
 bool Game::checkLoseCondition() const {
-    return money_ == 0 || current_day_ >= days_;
+    return money_ == 0 || (current_day_ >= days_ && money_ < final_goal_);
 }
