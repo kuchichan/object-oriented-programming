df159c0ac399fa7636b9bd13410af416402296e9
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-07-06 00:29:41 +0200

    added operator<< to island

--- shm/include/island.hpp
+++ shm/include/island.hpp
@@ -6,6 +6,8 @@ class Island {
 public:
     Island(int positionX, int positionY);
     Coordinates getCoordinates() const { return position_; }
+    friend std::ostream & operator<<(std::ostream & stream, const Island& island);
+
 
 private:
     Coordinates position_;
--- shm/src/island.cpp
+++ shm/src/island.cpp
@@ -1,3 +1,8 @@
 #include "island.hpp"
 
 Island::Island(int positionX, int positionY) : position_(positionX, positionY) {}
+
+std::ostream & operator<<(std::ostream & stream, const Island& island) {
+    stream << "Island coordinates: " << island.getCoordinates();
+    return stream;
+}
