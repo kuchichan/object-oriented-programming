5f4ade5becfeb05144cbc5ef6d67316448c0dec5
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-07-06 00:30:22 +0200

    added travel & getDistanceToIsland f-ction to map

--- shm/include/map.hpp
+++ shm/include/map.hpp
@@ -11,6 +11,7 @@ public:
     const std::vector<Island>& getAllIslands() { return Islands_; };
     void travel(Island* destination);
     size_t getDistanceToIsland(Island* destination);
+    Island* getCurrentPosition() { return currentPosition_; };
 
 
 private:
