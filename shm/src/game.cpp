e05f5858fd4e9eeb81d4b7ebd6d7165b8c61fcd6
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-07-06 12:28:29 +0200

    changing player initialization in game.cpp

--- shm/src/game.cpp
+++ shm/src/game.cpp
@@ -18,8 +18,7 @@ Game::Game(size_t money, size_t days, size_t final_goal)
       final_goal_(final_goal),
       time_(std::make_unique<Time>()) {
     map_ = std::make_unique<Map>();
-    player_ = std::make_unique<Player>(
-        std::make_unique<Ship>(10, 10, 10, player_.get()), money);
+    player_ = std::make_unique<Player>(money);
     buy_command_ = std::make_unique<Buy>(map_.get());
     sell_command_ = std::make_unique<Sell>(map_.get());
     travel_command_ = std::make_unique<Travel>(map_.get(), TimeServiceLocator::getTime());
