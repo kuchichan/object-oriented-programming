4e867fb2b83cd3728cafe8ffe49b33ad8251ed38
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-07-06 12:02:31 +0200

    testing travel feature

--- shm/src/main.cpp
+++ shm/src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
+#include "Command.hpp"
 #include "TimeServiceLocator.hpp"
+#include "Travel.hpp"
 #include "alcohol.hpp"
 #include "cargo.hpp"
+#include "map.hpp"
+#include "player.hpp"
 #include "ship.hpp"
-int main() {
-    // std::cout << "hi before\n";
-    // Alcohol alco("alco", 1, 2, 3);
-    // TimeServiceLocator::getTime().addObserver(&alco);
-    // std::cout << TimeServiceLocator::getTime().getElapsedTime() << std::endl;
-    // std::cout << "hi after\n";
 
+int main() {
+    Player player_one(100);
+    Map map_one;
+    Travel our_travel(&map_one, TimeServiceLocator::getTime());
+    our_travel.execute(&player_one);
     return 0;
 }
