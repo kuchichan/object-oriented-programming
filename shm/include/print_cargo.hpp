793f22decbcbd75369a48edd9641c9cd297800ce
kuchichan <pawel.kucharski@hotmail.com>
2020-07-16 20:40:58 +0200

    Splitting execute for cargo

--- shm/include/print_cargo.hpp
+++ shm/include/print_cargo.hpp
@@ -3,10 +3,15 @@
 #include "command.hpp"
 
 class Player;
+class Cargo;
 
 class PrintCargo : public Command {
 public:
     // Overrides from ICommand
     ~PrintCargo() override = default;
     void execute(Player* player) override;
+
+private:
+    void displayTableHeader();
+    void displayCargoValues(const Cargo* cargo);
 };
--- shm/src/print_cargo.cpp
+++ shm/src/print_cargo.cpp
@@ -4,20 +4,27 @@
 #include "player.hpp"
 #include "print_cargo.hpp"
 
-void PrintCargo::execute(Player* player) {
-    size_t cargoIndex = 0;
+void PrintCargo::displayCargoValues(const Cargo* cargo) {
+    std::cout << std::setw(12) << cargo->getName() << " |" << std::setw(12)
+              << cargo->getAmount() << " |" << std::setw(12) << cargo->getBasePrice()
+              << " |\n";
+}
+
+void PrintCargo::displayTableHeader() {
     std::string dash(42, '-');
-    auto cargo = player->getCargo(cargoIndex);
-    
     std::cout << std::setw(14) << "Name |" << std::setw(14) << " Amount |"
               << std::setw(15) << " Baseprice |\n"
               << dash << '\n';
+}
+
+void PrintCargo::execute(Player* player) {
+    size_t cargoIndex = 0;
+    auto cargo = player->getCargo(cargoIndex);
+
+    displayTableHeader();
 
     while (cargo) {
-        std::cout << std::setw(12) << cargo->getName() << " |" << std::setw(12)
-                  << cargo->getAmount() << " |" << std::setw(12) << cargo->getBasePrice()
-                  << " |\n";
-
+        displayCargoValues(cargo);
         cargoIndex++;
         cargo = player->getCargo(cargoIndex);
     }
